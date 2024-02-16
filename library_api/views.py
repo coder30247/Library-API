756edf83480ed57f60aa4d28537bc3cf53d99aef
Prathap Bharman <prathap24a@gmail.com>
2024-02-17 01:40:04 +0530

    created a bug free model

--- library_api/views.py
+++ library_api/views.py
@@ -12,6 +12,7 @@ class Student_Data_View(viewsets.ModelViewSet):
     queryset = Student_Data.objects.all()
     serializer_class = Student_Data_Serializer
 
+
 class Borrowed_Book_View(viewsets.ModelViewSet):
     queryset = Borrowed_Book.objects.all()
     serializer_class = Borrowed_Book_Serializer
