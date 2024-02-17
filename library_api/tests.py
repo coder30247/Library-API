3acc6533d7dec7e77b2a518efdc2f42187ee7743
Prathap Bharman <prathap24a@gmail.com>
2024-02-17 19:41:14 +0530

    perfectly completed the R operation

--- library_api/tests.py
+++ library_api/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from rest_framework.test import APIRequestFactory
+from rest_framework.test import APIClient
+from rest_framework.response import Response
 
+from .models import *
+from .views import *
 # Create your tests here.
+
--- library_api/views.py
+++ library_api/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import *
 from .serializers import *
@@ -11,30 +13,30 @@ def home(request):
 class Student_Data_View(viewsets.ModelViewSet):
     queryset = Student_Data.objects.all()
     serializer_class = Student_Data_Serializer 
-    
 
-    def list(self, request, *args, **kwargs):
-        # Get the original response from the parent class
-        response = super().list(request, *args, **kwargs)
+    @action(detail=True, methods=['get'])
+    def retrieve_student_data(self, request, pk=None):
+        # Retrieve the student instance
+        student_instance = self.get_object()
 
-        # Enhance the response by adding borrowed books for each student
-        student_data = response.data
-        enhanced_data = []
+        # Serialize the student data
+        student_data = Student_Data_Serializer(student_instance).data
 
-        for student in student_data:
-            student_instance = Student_Data.objects.get(pk=student['register_number'])
-            borrowed_books = Borrowed_Book.objects.filter(borrower=student_instance)
-            borrowed_books_data = Borrowed_Book_Serializer(borrowed_books, many=True).data
+        # Get the borrowed books for the student
+        borrowed_books = Borrowed_Book.objects.filter(borrower=student_instance)
+        
+        # Serialize the borrowed books data with additional details
+        borrowed_books_data = []
+        for book in borrowed_books:
+            book_data = Borrowed_Book_Serializer(book).data
+            book_data['due_date'] = book.return_date  # Add the due date (replace with the actual field name)
+            book_data['fine_amount'] = book.fine_amount  # Add the fine amount (replace with the actual field name)
+            borrowed_books_data.append(book_data)
 
-            student['borrowed_books'] = borrowed_books_data
-            enhanced_data.append(student)
+        # Add borrowed books data to the student data
+        student_data['borrowed_books'] = borrowed_books_data
 
-        # Update the response data
-        response.data = enhanced_data
-
-        return response
-
-
+        return Response(student_data)
 class Borrowed_Book_View(viewsets.ModelViewSet):
     queryset = Borrowed_Book.objects.all()
     serializer_class = Borrowed_Book_Serializer
