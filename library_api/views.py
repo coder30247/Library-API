5d7bea0b8dfd4b24b429c48c15109af814f0a283
Prathap Bharman <prathap24a@gmail.com>
2024-02-17 11:47:28 +0530

    finalized the R operation

--- library_api/views.py
+++ library_api/views.py
@@ -10,7 +10,29 @@ def home(request):
 
 class Student_Data_View(viewsets.ModelViewSet):
     queryset = Student_Data.objects.all()
-    serializer_class = Student_Data_Serializer
+    serializer_class = Student_Data_Serializer 
+    
+
+    def list(self, request, *args, **kwargs):
+        # Get the original response from the parent class
+        response = super().list(request, *args, **kwargs)
+
+        # Enhance the response by adding borrowed books for each student
+        student_data = response.data
+        enhanced_data = []
+
+        for student in student_data:
+            student_instance = Student_Data.objects.get(pk=student['register_number'])
+            borrowed_books = Borrowed_Book.objects.filter(borrower=student_instance)
+            borrowed_books_data = Borrowed_Book_Serializer(borrowed_books, many=True).data
+
+            student['borrowed_books'] = borrowed_books_data
+            enhanced_data.append(student)
+
+        # Update the response data
+        response.data = enhanced_data
+
+        return response
 
 
 class Borrowed_Book_View(viewsets.ModelViewSet):
